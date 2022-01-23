3ba4ea279388c4800642e42bb043b3746995240f
juangw <juangw@DESKTOP-JCJ77FB>
2022-01-23 18:43:28 -0500

    Add project files.

---
+++ c-sharp-kafka/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace c_sharp_kafka
+{
+    class Program
+    {
+        public static Task Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("Missing arguement to start Consumer or Producer");
+            }
+
+            string mode = args[0];
+            switch (mode.ToLower())
+            {
+                case "consumer":
+                    Console.WriteLine("Creating Consumers & Consuming Message");
+                    ConsumeMessage();
+                    return Task.CompletedTask;
+                case "producer":
+                    Console.WriteLine("Creating Producer & Producing Message");
+                    return ProduceMessage();
+                default:
+                    throw new ArgumentException($"Run Mode Provided: {mode}, Must be either `consumer` or `producer`");
+            }
+
+        }
+
+        private static async Task ProduceMessage()
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = "test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+
+        private static void ConsumeMessage()
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("test-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
